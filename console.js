76403416595e431aff5f5d7df0daef47ea1d75d0
Irina <irinagmarques@hotmail.com>
2020-08-04 14:22:31 +0200

    solution

---
+++ console.js
@@ -0,0 +1,28 @@
+// Console log a line of text eg. "this is an exercise in console logging" (do not forget quotation marks).
+console.log("This is an exercise in console logging");
+
+// Declare a variable and give your variable the following value: "This is the value.". Console log your variable.
+let variableOne = "This is the value";
+console.log(variableOne);
+
+// Declare a variable called firstName and assign your first name to that variable. Do the same with your last name. Console log both variables. NOTE: When console logging two variables, separate the variables with a comma (,) in the console log e.g. console.log(variable1, variable2);
+
+let firstName = "Irina";
+let lastName = "Marques";
+console.log(firstName , lastName);
+
+// Make a profile of someone's information. Store all the information in variables eg. name, city, job etc. (five variables total) Console log the variables.
+
+let first_name = "Peter";
+console.log(first_name);
+let last_name = "Johnson";
+console.log(last_name);
+let city = "Berlin";
+console.log(city);
+let job = "designer"
+console.log(job);
+let age = "30"
+console.log(age);
+
+// Print a sentence using the profile variables above, adding text where necessary for sentence flow, i.e., "John Smith is a 43 year old teacher who lives in Berlin". you can combine variables with text in the console by using the plus symbol (+) e.g. console.log("this is a " + variable);
+console.log(first_name , last_name , "is a" , age , job , "who lives in" , city , ".");
