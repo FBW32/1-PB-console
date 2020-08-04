1383a9bda097c84d79860ce332b20b67b665d272
Abayomi <mrbayus2014@gmail.com>
2020-08-04 15:38:47 +0200

    bayo

---
+++ console.js
@@ -0,0 +1,20 @@
+// 04.08.2020
+
+console.log("this " + " is" + " an" + " exercise" + " in" + " console" + " exercise");
+
+let request = "This is a value";
+console.log(request);
+
+let firstName = 'Abayomi';
+let lastName = 'Muritala';
+console.log(firstName, lastName);
+
+let name = 'James';
+let city = 'Berlin';
+let job = 'engineer';
+let hobby = 'reading';
+let phoneNumber = '0123456789';
+console.log(name, city, job, hobby, phoneNumber);
+
+
+console.log("name " + "city " + "job " + "hobby " + "phoneNumber”);
