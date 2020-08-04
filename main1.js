000229d12dc278259330817e4818a388973cc816
LawrenzoDM <lawrenzodelmc@outlook.com>
2020-08-04 16:28:38 +0200

    adding variables

---
+++ main1.js
@@ -0,0 +1,29 @@
+console.log("this is my exercise on console logging") //this is my exercise on the console logging.
+
+var myMusic = "this is the value";  
+console.log(myMusic);  //this is the value.
+
+var firstName = "Mbi";
+var lastName = "Lawrence";
+console.log(firstName,lastName);  //Mbi Lawrence.
+
+var firstName = "Jack";
+console.log(firstName);
+
+var lastName = "Powerman";  //powerman
+console.log(lastName);
+
+var skill = "driver";
+console.log(skill);    //driver
+
+var age = 35;
+console.log(age);  //35
+
+var location = "Frankfurt";
+console.log(location);     // frankfurt
+
+console.log(firstName,lastName + " is a " + age + " years old " + " prefessional F1 racing " + skill + " who lives in " + location);
+
+
+
+
