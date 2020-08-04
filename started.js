86605643e303ec81302311aeffcd5380a95f28fe
Maja <majamarek.design@gmail.com>
2020-08-04 14:30:44 +0200

    done

---
+++ started.js
@@ -0,0 +1,44 @@
+var text = "This is an exercise in console logging";
+console.log(text);
+
+var variableOne = "This is the value.";
+console.log(variableOne);
+
+let firstName = "Maja";
+let lastName = " Marek";
+let age = "40 Years old ";
+console.log(firstName, lastName, age);
+
+let country = " Polen ";
+console.log(country);
+
+let city = " ZG ";
+console.log(city);
+
+let job = "Teacher";
+console.log(job);
+
+let WE = "work exeriance";
+console.log(WE);
+
+let workExeriance = "20 years";
+console.log(workExeriance);
+
+console.log(
+  "This is a " +
+    firstName +
+    lastName +
+    " she is " +
+    age +
+    "and comes from" +
+    city +
+    "in" +
+    country +
+    " where she is a " +
+    job +
+    " with " +
+    workExeriance +
+    " of " +
+    WE +
+    "."
+);
