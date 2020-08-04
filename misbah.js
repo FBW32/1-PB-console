368f8aedbef43df58147d238f2c53815a1ee3b32
Misbah-D <misbah.deeb@gmail.com>
2020-08-04 13:56:01 +0200

    Done

---
+++ misbah.js
@@ -0,0 +1,16 @@
+let myFirstText = "this is an exercise in console logging";
+console.log(myFirstText);
+let variableOne = "This is the value.";
+console.log(variableOne);
+let firstName = "Misbah";
+let lastName = "Deeb";
+console.log(firstName, lastName);
+
+let firstNames = "John";
+let lastNames = "Luck";
+let cityName = " From Berlin"
+let theAge = "His Age 43"
+let workingJob = "Working as a Teacher"
+console.log(firstNames, lastNames, cityName, theAge, workingJob);
+
+
