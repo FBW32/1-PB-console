bfcb334b04f6a6ebb1676b1fbf47dd55e0972b29
shanti-s <surapanenishanti9@gmail.com>
2020-08-04 13:58:35 +0200

    Added

---
+++ exercise1.js
@@ -0,0 +1,19 @@
+console.log("this is an exercise in console logging");
+
+let myVal = "This is the value.";
+console.log(myVal);
+
+let firstName = "Shanti";
+let lastName = "Surapaneni";
+console.log(firstName, lastName);
+
+const fName = "Dave";
+const lName = "Williams";
+const city = "Berlin";
+const job = "Teacher";
+const age = "31";
+console.log(fName, lName, city, job, age);
+
+console.log("John Smith " + "is " + "a " + "43 " + "year " +
+            "old " + "teacher " + "who " + "lives " + "in " +
+            "Berlin");
