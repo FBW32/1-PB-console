23f299a3cb48556130c51fea01ab549c6137aba2
anika555 <ak15@live.de>
2020-08-04 14:45:30 +0200

    first exercise

---
+++ exe1/anika/exe1.js
@@ -0,0 +1,35 @@
+// ************Exercise**************
+
+console.log("Hello, I'm doing the exercise now");
+
+let myVar = "This is the value.";
+console.log(myVar);
+
+let firstName = "Anika";
+console.log(firstName);
+
+let lastName = "König";
+console.log(lastName);
+
+let name = "Hannah Builders";
+let age = "30";
+let city = "Berlin";
+let job = "Web Developer";
+let faveBand = "The Kooks";
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(job);
+console.log(faveBand);
+
+console.log(
+  name +
+    " is taking a break from her job as a " +
+    job +
+    " to go to a concert of her favourite band " +
+    faveBand +
+    " in her home town " +
+    city +
+    "."
+);
