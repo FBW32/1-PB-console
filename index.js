8b415f133d619b33a12143a45ec8a583c76f5b27
Aiham <aiham.rabee@gmail.com>
2020-08-05 15:35:37 +0200

    ok

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("this is an exercise in console logging");
+
+let theFirst = "This is the value.";
+console.log("This is the value.");
+
+let firstName = "Aiham";
+let lastName = "Rabee";
+console.log(firstName, lastName);
+
+let n = "Etapp kyle";
+let a = "30";
+let c = "Ukraine";
+let j = "DJ";
+let rl = "Ostgut ton";
+console.log("Name: "+n,);
+console.log("Age: "+a);
+console.log("Country: "+c,);
+console.log("Job: "+j,);
+console.log("Record lable: "+rl,);
+
+console.log("This is a story of " + n + " " + ".");
+console.log(
+  "He's " + a + " years old.");
+console.log(
+  "He from " +
+    c );
+console.log("Now work as a " + j + " in Berlin.");
+console.log("With " + rl);
